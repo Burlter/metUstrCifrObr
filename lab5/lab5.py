4e3b28abbffdad67e2398665343aa43d4041ca8a
guapstudent1337 <guapstudent1337@users.noreply.github.com>
2020-11-20 21:14:00 +0000

    wiener filter final

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,86 +1,38 @@
-def plot_signal_difference(signal_delta, signal_stdev):
+def plot_signal_difference(exponential_signal_delta, signal_stdev):
     """
     Функция выводит график разности между сигналами и среднеквадратическую
     ошибку фильтрации
 
-    :signal_delta: Разность сигнала
+    :exponential_signal_delta: Разность сигнала
     :signal_stdev: Среднеквадратическая ошибка сигнала
     """
     import matplotlib.pyplot as plt
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(signal_delta)
+    ax.plot(exponential_signal_delta)
     ax.set_title("Разность зашумлённого и аппроксимированного сигналов")
     ax.text(0, 0.5, "Среднеквадратическая ошибка аппроксимации равна " +
             "{}".format(round(signal_stdev, 3)), transform=ax.transAxes,
             fontsize=14)
 
 
-def approximate_signal_with_basis(window_length, signal_for_smooth, basis):
-    """
-    Сглаживает сигнал методом наименьших квадратов с помощью скользящего окна
+def wiener_signal_filter(signal_length, signal_that_need_to_filter_length,
+                         wiener_coefficients, noised_signal):
+    """Signal filtration with wiener filter"""
+    from numpy import array
 
-    :window_length: Длинна скользящего окна
-    :signal_for_smooth: Сигнал для сглаживания
-    :basis: базис
-    :return: фильтрованный сигнал
-    """
-    import numpy as np
+    filtered_signal = []
+    for number in range(0, signal_length):
+        filtered_signal_step_sum = 0
+        for sum_number in range(0, signal_that_need_to_filter_length):
+            if number - sum_number >= 0:
+                filtered_signal_step_sum += (
+                    wiener_coefficients[sum_number]
+                    * noised_signal[number - sum_number])
+        filtered_signal.append(filtered_signal_step_sum)
 
-    approximated_signal = []
-    for number in range(0, len(signal_for_smooth)):
-
-        # Двигаем окно
-        if number < int(window_length/2):
-            cutted_signal = np.zeros(window_length)
-
-            for signal_number in range(int(window_length/2) - number,
-                                       window_length):
-                cutted_signal[signal_number] = signal_for_smooth[
-                        signal_number - int(window_length/2) + number
-                        ]
-
-        elif number > (len(signal_for_smooth) - int(window_length/2)
-                       - (window_length % 2)):
-            cutted_signal = np.zeros(window_length)
-            for signal_number in range(0, int(window_length/2)
-                                       + (len(signal_for_smooth) - number)):
-                cutted_signal[signal_number] = signal_for_smooth[
-                        signal_number + number - int(window_length/2)]
-        else:
-            cutted_signal = signal_for_smooth[
-                    number - int(window_length/2):
-                    number + int(window_length/2) + window_length % 2]
-        cutted_signal = np.array(cutted_signal)
-
-        # Находим коэффициент разложения
-        transpose_basis = basis.transpose()
-        coefficient_of_decomposition = np.matmul(np.linalg.inv(np.matmul(
-            transpose_basis, basis)),
-            np.matmul(transpose_basis, cutted_signal))
-
-        # Осуществляем аппроксимацию на основе базисной функции
-        approximated_signal.append(sum(
-            np.multiply(coefficient_of_decomposition,
-                        basis[int(window_length/2)
-                              + window_length % 2])))
-
-    return np.array(approximated_signal)
-
-
-def least_squares_polinomial_generate(window_length, basis_order_range):
-    """Генерирует мнк полином с заданной длинной окна и порядком базиса"""
-    import numpy as np
-
-    moving_window_range = np.linspace(0, window_length - 1, window_length)
-
-    least_squares_array = []
-    for order in basis_order_range:
-        least_squares_array.append(
-                (moving_window_range/np.sqrt(window_length))**order)
-
-    return np.array(least_squares_array).transpose()
+    return array(filtered_signal)
 
 
 def main():
@@ -94,11 +46,13 @@ def main():
     signal_discretization = 1/useful_signal_length
     useful_signal_range = np.linspace(0, useful_signal_length - 1,
                                       useful_signal_length)
-    useful_signal = (
-        cos(pi * useful_signal_range * signal_discretization /
-            useful_signal_length)
-        - cos(3 * pi * useful_signal_range * signal_discretization /
-              useful_signal_length))
+    #useful_signal = (
+    #    cos(pi * useful_signal_range * signal_discretization /
+    #        useful_signal_length)
+    #    - cos(3 * pi * useful_signal_range * signal_discretization /
+    #          useful_signal_length))
+    useful_signal = (cos(2 * pi * useful_signal_range * 3
+                     * signal_discretization))**3
 
     # Plot useful signal
     plt.figure()
@@ -108,7 +62,7 @@ def main():
     plt.ylabel("Амплитуда")
 
     # Set parametrs for main signal and generate him
-    main_signal_length = 15000
+    main_signal_length = 1500
     shift_size = 500
     main_signal = []
     for number in range(0, main_signal_length):
@@ -116,10 +70,11 @@ def main():
             main_signal.append(useful_signal[number - shift_size])
         else:
             main_signal.append(0)
+    main_signal = np.array(main_signal)
 
     # Generate gaussian noise
-    wishful_stdev = 1
-    gaussian_noise = np.random.normal(0, wishful_stdev, main_signal_length)
+    noise_stdev = 0.4
+    gaussian_noise = np.random.normal(0, noise_stdev, main_signal_length)
     gaussian_noise_signal = main_signal + gaussian_noise
 
     # Generate exponential correlation function noise, which formula
@@ -129,12 +84,13 @@ def main():
     exponential_operator = (
         distribution_of_a_random_variable * signal_discretization)
     noise_exp = exp(-exponential_operator)
-    exponentional_noise = [wishful_stdev * np.sqrt(1 - noise_exp**2) *
-                           gaussian_noise[number]]
+    gaussian_noise_for_exp = np.random.normal(0, 1, main_signal_length)
+    exponentional_noise = [noise_stdev * np.sqrt(1 - noise_exp**2) *
+                           gaussian_noise_for_exp[number]]
     for number in range(1, main_signal_length):
         exponentional_noise.append(
                 exponentional_noise[number - 1] * noise_exp
-                + gaussian_noise[number] * wishful_stdev * np.sqrt(
+                + gaussian_noise_for_exp[number] * noise_stdev * np.sqrt(
                     1 - noise_exp**2))
     exponentional_noise = np.array(exponentional_noise)
 
@@ -146,9 +102,9 @@ def main():
     correlation_shift_range = np.linspace(0, main_signal_length,
                                           main_signal_length)
     theoretical_exponentional_noise_correlation = (
-        wishful_stdev**2 * np.exp(-distribution_of_a_random_variable
-                                  * correlation_shift_range
-                                  * signal_discretization))
+        noise_stdev**2 * np.exp(-distribution_of_a_random_variable
+                                * correlation_shift_range
+                                * signal_discretization))
 
     # Plot theoretical and practical correlation
     plt.figure()
@@ -163,11 +119,12 @@ def main():
 
     exponentional_noise_signal = main_signal + exponentional_noise
 
-    expected_value = sum(useful_signal) / useful_signal_length
+    # Find coeficients for gaussian noise signal filter
     usful_signal_correlation = []
+    expected_value = sum(useful_signal) / useful_signal_length
     for number in useful_signal_range:
         usful_signal_correlation_step_sum = 0
-        for sum_number in range(0, useful_signal_length - int(number) - 1):
+        for sum_number in range(0, useful_signal_length - int(number)):
             usful_signal_correlation_step_sum += (((useful_signal[sum_number]
                                                     - expected_value)
                                                    * (useful_signal[
@@ -175,7 +132,7 @@ def main():
                                                        + int(number)]
                                                       - expected_value)))
         usful_signal_correlation.append(usful_signal_correlation_step_sum
-                                        / useful_signal_length)
+                                        / (useful_signal_length))
     usful_signal_correlation = np.array(usful_signal_correlation)
 
     autocorrelation_useful_signal_matrix = []
@@ -183,7 +140,7 @@ def main():
         autocorrelation_useful_signal_matrix.append([])
         for j in useful_signal_range:
             autocorrelation_useful_signal_matrix[int(i)].append(
-                usful_signal_correlation[int(i) - int(j)])
+                usful_signal_correlation[abs(int(i) - int(j))])
     autocorrelation_useful_signal_matrix = np.array(
         autocorrelation_useful_signal_matrix)
 
@@ -193,7 +150,7 @@ def main():
         for j in useful_signal_range:
             if i == j:
                 autocorrelation_gaussian_noise_matrix[int(i)].append(
-                    wishful_stdev**2)
+                    noise_stdev**2)
             else:
                 autocorrelation_gaussian_noise_matrix[int(i)].append(0)
     autocorrelation_gaussian_noise_matrix = np.array(
@@ -202,90 +159,59 @@ def main():
     cross_correlation_matrix = (autocorrelation_gaussian_noise_matrix
                                 + autocorrelation_useful_signal_matrix)
 
-    wiener_coefficients = (np.matmul(np.linalg.inv(cross_correlation_matrix),
-                                     usful_signal_correlation))
+    gaussian_noise_wiener_coefficients = (np.matmul(np.linalg.inv(
+        cross_correlation_matrix), usful_signal_correlation))
 
-    filtered_signal = []
-    for number in range(0, main_signal_length):
-        filtered_signal_step_sum = 0
-        for sum_number in useful_signal_range:
-            if number - sum_number >= 0:
-                filtered_signal_step_sum += (
-                    wiener_coefficients[int(sum_number)] 
-                    * gaussian_noise_signal[number - int(sum_number)]) 
-        filtered_signal.append(filtered_signal_step_sum)
-    
-    print(gaussian_noise_signal[0].size, wiener_coefficients[0].size, 
-    wiener_coefficients.size, cross_correlation_matrix[3].size)
-    
+    gaussian_noise_filtered_signal = wiener_signal_filter(
+        main_signal_length, useful_signal_length,
+        gaussian_noise_wiener_coefficients, gaussian_noise_signal)
+
     plt.figure()
-    plt.plot(filtered_signal)
+    plt.plot(gaussian_noise_signal, '--', label="Зашумлённый сигнал")
+    plt.plot(main_signal, label="Сигнал без шума")
+    plt.plot(gaussian_noise_filtered_signal, label="Фильтрованный сигнал",
+             linewidth=3)
+    plt.legend()
 
+    exponential_signal_delta = main_signal - gaussian_noise_filtered_signal
+    exponential_signal_stdev = (stdev(exponential_signal_delta)
+                                / stdev(main_signal))
+    plot_signal_difference(exponential_signal_delta, exponential_signal_stdev)
 
+    # Find coeficients for filter signal with exponential noise
+    autocorrelation_exponential_noise_matrix = []
+    for i in useful_signal_range:
+        autocorrelation_exponential_noise_matrix.append([])
+        for j in useful_signal_range:
+            if i == j:
+                autocorrelation_exponential_noise_matrix[int(i)].append(
+                    noise_stdev**2 * noise_exp)
+            else:
+                autocorrelation_exponential_noise_matrix[int(i)].append(0)
+    autocorrelation_exponential_noise_matrix = np.array(
+        autocorrelation_exponential_noise_matrix)
 
+    cross_correlation_matrix = (autocorrelation_exponential_noise_matrix
+                                + autocorrelation_useful_signal_matrix)
 
+    exponential_noise_wiener_coefficients = (np.matmul(np.linalg.inv(
+        cross_correlation_matrix), usful_signal_correlation))
 
-    ## Задаём константные значения для базисной функции
-    #order_max = 3
-    #order_range = np.linspace(0, order_max - 1, order_max)
-    #moving_window_max = 380
+    exponentional_noise_filtered_signal = wiener_signal_filter(
+        main_signal_length, useful_signal_length,
+        exponential_noise_wiener_coefficients, exponentional_noise_signal)
 
-    ## Находим зависимость среднеквадратической ошибки от длинны окна и
-    ## вычисляем индекс минимальной среднеквадратической ошибки.
-    #stdev_array = []
-    #plot_start = 280
-    #window_range = range(plot_start, moving_window_max)
-    #for window_number in window_range:
-    #    # Вычисляем полином для конкретного размера окна
-    #    polinominal = least_squares_polinomial_generate(window_number,
-    #                                                    order_range)
+    plt.figure()
+    plt.plot(exponentional_noise_signal, '--', label="Зашумлённый сигнал")
+    plt.plot(main_signal, label="Сигнал без шума")
+    plt.plot(exponentional_noise_filtered_signal, label="Фильтрованный сигнал",
+             linewidth=2)
+    plt.legend()
 
-    #    # Аппроксимируем сигнал полученным полиномом
-    #    approximated_signal = approximate_signal_with_basis(
-    #            window_number, gaussian_noise_signal, polinominal)
-
-    #    # Вычисляем среднеквадратическую ошибку
-    #    signal_delta = generated_signal - approximated_signal
-    #    signal_stdev = stdev(signal_delta)/stdev(generated_signal)
-    #    stdev_array.append(signal_stdev)
-
-    #index_of_minimum_stdev = stdev_array.index(min(stdev_array)) + plot_start
-
-    ## Рисуем график зависимости
-    #plt.figure()
-    #plt.xlabel("Порядки базиса")
-    #plt.ylabel("Величина среднеквадратической ошибки")
-    #plt.plot(window_range, stdev_array)
-    #plt.text(plot_start, min(stdev_array), "Наименьшее значение " +
-    #         "среднеквадратической ошибки = " +
-    #         "{}".format(round(min(stdev_array), 3)), fontsize=12)
-    #plt.text(plot_start, min(stdev_array) + 0.001, "Порядок наименьшего" +
-    #         "значения среднеквадратической ошибки = " +
-    #         "{}".format(index_of_minimum_stdev), fontsize=12)
-
-    ## Аппроксимируем наилучшей длинной окна, которую мы узнали ранее
-    ## Снова формируем полином
-    #polinominal = least_squares_polinomial_generate(index_of_minimum_stdev,
-    #                                                order_range)
-
-    ## Аппроксимируем
-    #approximated_signal = approximate_signal_with_basis(index_of_minimum_stdev,
-    #                                                    gaussian_noise_signal,
-    #                                                    polinominal)
-    #signal_delta = generated_signal - approximated_signal
-    #signal_stdev = stdev(signal_delta)/stdev(generated_signal)
-
-    ## Рисуем графики сигналов
-    #plt.figure()
-    #plt.title("Сравнение зашумлённого сигнала и фильтрованного аппроксимацией")
-    #plt.xlabel("Время")
-    #plt.ylabel("Амплитуда")
-    #plt.plot(time, gaussian_noise_signal, label="Зашумлённый сигнал")
-    #plt.plot(time, approximated_signal, label="Аппроксимированный сигнал")
-    #plt.legend()
-
-    ## Выводим график разности сигналов
-    #plot_signal_difference(signal_delta, signal_stdev)
+    exponential_signal_delta = main_signal - gaussian_noise_filtered_signal
+    exponential_signal_stdev = (stdev(exponential_signal_delta)
+                                / stdev(main_signal))
+    plot_signal_difference(exponential_signal_delta, exponential_signal_stdev)
 
     plt.show()
 
